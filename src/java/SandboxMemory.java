4880711b6526ef915859db5e38686f4606637396
Nicolas Hoibian <nicolas.hoibian@gmail.com>
2010-09-19 22:54:22 +0200

    Found a configuration that allows rendering of up to 108 megapixels with 512MB Xmx / Xms

--- src/java/SandboxMemory.java
+++ src/java/SandboxMemory.java
@@ -21,22 +21,38 @@ public class SandboxMemory {
 		// TODO Auto-generated method stub
 		File pdf = new File(args[0]);
 		System.out.println("Selected pdf on which to run the test : " + args[0]);
-		int side = 1700;
+		int side = 12000;
 		System.out.println("Page sizes for max size = " + side);
-		for (int n = 1; n < 50; n++) {
-			PDFToImageRendererPixels ren = new PDFToImageRendererPixels();
-			ren.setPDFFromFile(pdf);
-			int pages = ren.getPageCount();
+		PDFToImageRendererPixels ren;
+		int pages = 0;
+		for (int n = 1; n < 10; n++) {
+			if (n == 1) {
+				ren = new PDFToImageRendererPixels();
+				ren.setPDFFromFile(pdf);
+				pages = ren.getPageCount();
+				Dimension d = ren.getImageDimForSideLength(1, side);
+				System.out.println("Dimension : " + d.width + " * " + d.height + " MP : " + ((double) (d.width * d.height)) / (1000.0 * 1000.0));
+			}
+			ren = null;
 			for (int i = 1; i <= pages; i++) {
+				ren = new PDFToImageRendererPixels();
+				ren.setPDFFromFile(pdf);
 				BufferedImage img = ren.getExtract(i, side, 200);
-				img=null;
+				img = null;
+				System.out.print("page : " + i + " Before reset : Memory infos : ");
+				TestMemory.printMemoryInfo();
+				ren = null;
 				System.gc();
+				Thread.sleep(1000);
+				System.gc();
+				System.out.print("page : " + i + " After reset  : Memory infos : ");
+				TestMemory.printMemoryInfo();
 			}
 			ren = null;
 			Thread.sleep(1000);
 			System.gc();
 			Thread.sleep(1000);
-			System.out.print("Iteration : " + n + " Memory infos : ");
+			System.out.print("After iteration : " + n + " Memory infos : ");
 			TestMemory.printMemoryInfo();
 		}
 	}
